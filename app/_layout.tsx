e64c6b2f1635baec4a5c3db72e3f46d88191f480
Rohan Poudel <yitsmerohan@gmail.com>
2026-03-29 13:08:15 +0545

    chore: update project configuration and root layout for theming

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,24 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+import { ThemeProvider } from "../context/theme-context";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
+        <Stack.Screen name="(main)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="(main)/transaction-detail"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="(main)/history-detail"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="(main)/profile" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
