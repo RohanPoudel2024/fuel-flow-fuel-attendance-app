8e8c8b8fc660f41b5482a96098097e1f9994af1a
Rohan Poudel <yitsmerohan@gmail.com>
2026-04-12 10:25:34 +0545

    chore: update attendance api config

--- config/api.config.ts
+++ config/api.config.ts
@@ -1,16 +1,23 @@
 import { Platform } from "react-native";
 
-const USE_NGROK = true;
-const NGROK_URL = "faye-subobsolete-uninferrably.ngrok-free.dev";
-const LOCAL_IP = "192.168.18.244";
+const PRODUCTION_API_URL = process.env.EXPO_PUBLIC_API_URL ?? "";
+const USE_NGROK = process.env.EXPO_PUBLIC_USE_NGROK === "true";
+const NGROK_URL = process.env.EXPO_PUBLIC_NGROK_URL ?? "";
+const LOCAL_IP = process.env.EXPO_PUBLIC_LOCAL_IP ?? "";
 
 const getApiUrl = () => {
+  if (PRODUCTION_API_URL) {
+    return PRODUCTION_API_URL;
+  }
+
   if (USE_NGROK && NGROK_URL) {
     return `https://${NGROK_URL}/api`;
   }
-  if (true) {
+
+  if (LOCAL_IP) {
     return `http://${LOCAL_IP}:3000/api`;
   }
+
   return Platform.select({
     ios: "http://localhost:3000/api",
     android: "http://10.0.2.2:3000/api",
